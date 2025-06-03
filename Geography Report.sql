c4bce7ab2ab617e4e6aaaf074f29245e066e964c
Meharsh Chandure <133075553+Meharsh7804@users.noreply.github.com>
2025-06-03 17:30:42 +0530

    Geography Report Database

---
+++ Geography Report.sql
@@ -0,0 +1,5 @@
+Create table If Not Exists student (name varchar(50), continent varchar(7));
+insert into student (name, continent) values ('Jane', 'America');
+insert into student (name, continent) values ('Pascal', 'Europe');
+insert into student (name, continent) values ('Xi', 'Asia');
+insert into student (name, continent) values ('Jack', 'America');
